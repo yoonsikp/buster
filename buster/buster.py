def2a728f3d2d407df108906fd7cd13457b6865c
Yoonsik Park <park.yoonsik@icloud.com>
2019-04-29 16:27:27 -0500

    Update buster.py

--- buster/buster.py
+++ buster/buster.py
@@ -1,11 +1,7 @@
 """Ghost Buster. Static site generator for Ghost.
 
 Usage:
-  buster.py setup [--gh-repo=<repo-url>] [--dir=<path>]
   buster.py generate [--domain=<local-address>] [--dir=<path>]
-  buster.py preview [--dir=<path>]
-  buster.py deploy [--dir=<path>]
-  buster.py add-domain <domain-name> [--dir=<path>]
   buster.py (-h | --help)
   buster.py --version
 
@@ -14,21 +10,12 @@ Options:
   --version                 Show version.
   --dir=<path>              Absolute path of directory to store static pages.
   --domain=<local-address>  Address of local ghost installation [default: localhost:2368].
-  --gh-repo=<repo-url>      URL of your gh-pages repository.
 """
-
 import os
 import re
-import sys
 import fnmatch
-import shutil
-import SocketServer
-import SimpleHTTPServer
 from docopt import docopt
-from time import gmtime, strftime
-from git import Repo
 from pyquery import PyQuery
-
 
 def main():
     arguments = docopt(__doc__, version='0.1.3')
@@ -68,10 +55,26 @@ def main():
                 if not abs_url_regex.search(href):
                     new_href = re.sub(r'rss/index\.html$', 'rss/index.rss', href)
                     new_href = re.sub(r'/index\.html$', '/', new_href)
+                    new_href = re.sub(r'index\.html$', '.', new_href)
                     e.attr('href', new_href)
                     print "\t", href, "=>", new_href
+            for element in d('link'):
+                e = PyQuery(element)
+                href = e.attr('href')
+                if href is not None and not abs_url_regex.search(href):
+                    new_href = re.sub(r'rss/index\.html$', 'rss/index.rss', href)
+                    new_href = re.sub(r'\?v=.*$', '', href)
+                    e.attr('href', new_href)
+                    print "\t", href, "removed v =>", new_href
+            for element in d('script'):
+                e = PyQuery(element)
+                href = e.attr('src')
+                if href is not None and not abs_url_regex.search(href):
+                    new_href = re.sub(r'\?v=.*$', '', href)
+                    e.attr('src', new_href)
+                    print "\t", href, "removed v =>", new_href
             if parser == 'html':
-                return d.html(method='html').encode('utf8')
+                return "<!DOCTYPE html>" + d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
 
         # fix links in all html files
@@ -90,75 +93,6 @@ def main():
                 newtext = fixLinks(filetext, parser)
                 with open(filepath, 'w') as f:
                     f.write(newtext)
-
-    elif arguments['preview']:
-        os.chdir(static_path)
-
-        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-        httpd = SocketServer.TCPServer(("", 9000), Handler)
-
-        print "Serving at port 9000"
-        # gracefully handle interrupt here
-        httpd.serve_forever()
-
-    elif arguments['setup']:
-        if arguments['--gh-repo']:
-            repo_url = arguments['--gh-repo']
-        else:
-            repo_url = raw_input("Enter the Github repository URL:\n").strip()
-
-        # Create a fresh new static files directory
-        if os.path.isdir(static_path):
-            confirm = raw_input("This will destroy everything inside static/."
-                                " Are you sure you want to continue? (y/N)").strip()
-            if confirm != 'y' and confirm != 'Y':
-                sys.exit(0)
-            shutil.rmtree(static_path)
-
-        # User/Organization page -> master branch
-        # Project page -> gh-pages branch
-        branch = 'gh-pages'
-        regex = re.compile(".*[\w-]+\.github\.(?:io|com).*")
-        if regex.match(repo_url):
-            branch = 'master'
-
-        # Prepare git repository
-        repo = Repo.init(static_path)
-        git = repo.git
-
-        if branch == 'gh-pages':
-            git.checkout(b='gh-pages')
-        repo.create_remote('origin', repo_url)
-
-        # Add README
-        file_path = os.path.join(static_path, 'README.md')
-        with open(file_path, 'w') as f:
-            f.write('# Blog\nPowered by [Ghost](http://ghost.org) and [Buster](https://github.com/axitkhurana/buster/).\n')
-
-        print "All set! You can generate and deploy now."
-
-    elif arguments['deploy']:
-        repo = Repo(static_path)
-        repo.git.add('.')
-
-        current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        repo.index.commit('Blog update at {}'.format(current_time))
-
-        origin = repo.remotes.origin
-        repo.git.execute(['git', 'push', '-u', origin.name,
-                         repo.active_branch.name])
-        print "Good job! Deployed to Github Pages."
-
-    elif arguments['add-domain']:
-        repo = Repo(static_path)
-        custom_domain = arguments['<domain-name>']
-
-        file_path = os.path.join(static_path, 'CNAME')
-        with open(file_path, 'w') as f:
-            f.write(custom_domain + '\n')
-
-        print "Added CNAME file to repo. Use `deploy` to deploy"
-
     else:
         print __doc__
 
